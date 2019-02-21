4e72cc00685252051b3a38def9b8a2421656c059
status <sinhropazatron@gmail.com>
2019-02-22 02:43:16 +0300

    hidden form on login page

--- python/assets/js/app.js
+++ python/assets/js/app.js
@@ -2,8 +2,17 @@ import Vue from 'vue'
 import SelectSearch from './components/SelectSearch.vue'
 
 new Vue({
-  el: '#signup_form',
+  el: '#opr-app',
   components: {
     SelectSearch
+  },
+  methods:{
+    show_form: function (e, form_class) {
+        e.target.parentElement.classList.add("method-auth-nickname-active-form");
+        var forms_hidden = document.getElementsByClassName(form_class);
+        while(forms_hidden.length > 0){
+            forms_hidden[0].classList.remove(form_class);
+        }
+    }
   }
 })
