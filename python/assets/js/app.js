d8fffd283e827deb823f79f5e5213441c63f9493
status <sinhropazatron@gmail.com>
2019-02-28 00:06:35 +0300

    expand blocks for sign up forms

--- python/assets/js/app.js
+++ python/assets/js/app.js
@@ -1,16 +1,21 @@
 import Vue from 'vue'
 import SelectSearch from './components/SelectSearch.vue'
+import ExpandBlock from './components/ExpandBlock.vue'
+import VShowSlide from 'v-show-slide'
+
+Vue.use(VShowSlide)
 
 new Vue({
   el: '#opr-app',
   components: {
-    SelectSearch
+    SelectSearch,
+    ExpandBlock
   },
   methods:{
     show_form: function (e, form_class) {
         e.target.parentElement.classList.add("method-auth-nickname-active-form");
         var forms_hidden = document.getElementsByClassName(form_class);
-        while(forms_hidden.length > 0){
+        while (forms_hidden.length > 0) {
             forms_hidden[0].classList.remove(form_class);
         }
     }
--- python/webpack.config.js
+++ python/webpack.config.js
@@ -18,6 +18,14 @@ module.exports = {
         test: /\.css$/,
         loaders: ['style-loader', 'css-loader']
       },
+      {
+        test: /\.scss$/,
+        use: [
+            "style-loader", // creates style nodes from JS strings
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      }
     ]
   },
   plugins: [
