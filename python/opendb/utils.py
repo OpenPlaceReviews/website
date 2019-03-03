403bc70694400c3fd409e0cca16f75dfb0ab5fdf
status <sinhropazatron@gmail.com>
2019-03-03 13:31:40 +0300

    Fix bug for signup/login messages

--- python/opendb/utils.py
+++ python/opendb/utils.py
@@ -23,9 +23,9 @@ def opendb_signup(nickname, pwd=None, oauth_provider=None,
     }
     response = requests.post(api_url, data=data)
     if response.status_code == 200:
-        logger.info('Signed user {}'.format(json.loads(response.content).get('name')))
+        logger.info('Signed user {}'.format(json.loads(response.content.decode('utf-8')).get('name')))
     else:
-        logger.error('Error signed on OpenDB: {}'.format(response.content))
+        logger.error('Error signed on OpenDB: {}'.format(response.content.decode('utf-8')))
     return response
 
 
@@ -46,5 +46,5 @@ def opendb_login(nickname, pwd=None, oauth_provider=None,
     if response.status_code == 200:
         logger.info('Signed user {}'.format(json.loads(response.content.decode('utf-8')).get('name')))
     else:
-        logger.error('Error signed on OpenDB: {}'.format(response.content))
+        logger.error('Error signed on OpenDB: {}'.format(response.content.decode('utf-8')))
     return response
