ee457be32a46a572cbb21fcec7ee4ddc95f02f66
status <sinhropazatron@gmail.com>
2019-03-04 18:34:10 +0300

    Change languages json and sorted it

--- python/profiles/models.py
+++ python/profiles/models.py
@@ -3,16 +3,17 @@ from django.contrib.auth.models import AbstractUser
 from .managers import UserManager
 from django.utils.translation import gettext_lazy as _
 from django.utils.functional import cached_property
-
+from operator import itemgetter
 import json
 
 countries_json = open('profiles/countries.json').read()
 countries_data = json.loads(countries_json)
 COUNTRIES = [[item.get('alpha-3'), item.get('name')] for item in countries_data]
 
-languages_json = open('profiles/iso_639-2_languages.json').read()
+languages_json = open('profiles/iso_639-1_languages.json').read()
 languages_data = json.loads(languages_json)
-LANGUAGES = [[k, v.get('en')[0]] for k, v in languages_data.items()]
+LANGUAGES = [[k, '{}-{}'.format(v.get('name'), v.get('native'))] for k, v in languages_data.items()]
+LANGUAGES = sorted(LANGUAGES, key=itemgetter(1))
 
 class User(AbstractUser):
     username = models.CharField(_('username'), max_length=255, unique=True)
