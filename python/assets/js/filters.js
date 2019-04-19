6f665232b234bad5eda3b463c5c46870029435ce
status <sinhropazatron@gmail.com>
2019-04-19 23:40:11 +0300

    datefomat ro UTC custom string

--- python/assets/js/filters.js
+++ python/assets/js/filters.js
@@ -1,4 +1,8 @@
+function two_digit(val) {
+   return (val < 10 ? '0' : '') + val;
+}
 export default {
+
     slice_hash: function(str, first=0, last=12) {
       return str.split(':').reverse()[0].slice(first, last)
     },
@@ -8,6 +12,22 @@ export default {
     date_to_locale_string: function (datetime) {
       datetime = new Date(datetime);
       return datetime.toLocaleString()
+    },
+    date_to_utc_string: function(datetime){
+        datetime = new Date(datetime);
+        return datetime.toUTCString();
+    },
+    date_to_utc_custom: function (datetime) {
+
+        var datetime = new Date(datetime);
+        var mm = two_digit(datetime.getUTCMonth());
+        var dd = two_digit(datetime.getUTCDay());
+        var hh = two_digit(datetime.getUTCHours());
+        var MM = two_digit(datetime.getUTCMinutes());
+        var ss = two_digit(datetime.getUTCSeconds());
+
+      var str_date = datetime.getUTCFullYear() + '-' + mm + '-' + dd + ' ' + hh + ':' + MM + ':' + ss + ' UTC';
+      return str_date;
     },
     replace_all: function (str, search, replace) {
         return str.split(search).join(replace);
