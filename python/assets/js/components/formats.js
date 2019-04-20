5a9dff6ca48840d848b18a650ed232170ca7a8d8
status <sinhropazatron@gmail.com>
2019-04-20 23:22:20 +0300

    add function getObjectName and getOperationsDescription

--- python/assets/js/components/formats.js
+++ python/assets/js/components/formats.js
@@ -14,7 +14,7 @@ var format = {
         else if (obj.type == 'sys.login' && obj.new && obj.new.length > 0 && (!obj.old || !obj.old.length == 0)) return 'Login';
         else if (obj.type == 'sys.login' && obj.new && obj.new.length > 0 && obj.old && obj.old.length > 0) return 'Edit Profile';
         else if (obj.type == 'sys.login' && (!obj.new || obj.new.length == 0) && obj.old && obj.old.length > 0) return 'Logout';
-        else if (obj.type == 'sys.grant' && obj.new && obj.new.length > 0 && (!obj.old || !obj.old.length == 0)) return 'Add premission';
+        else if (obj.type == 'sys.grant' && obj.new && obj.new.length > 0 && (!obj.old || !obj.old.length == 0)) return 'Add permission';
         else if (obj.type == 'sys.grant' && obj.new && obj.new.length > 0 && obj.old && obj.old.length > 0) return 'Edit permission';
         else if (obj.type == 'sys.grant' && (!obj.new || obj.new.length == 0) && obj.old && obj.old.length > 0) return 'Delete permission';
         else if (obj.type == 'sys.operation' && obj.new && obj.new.length > 0 && (!obj.old || !obj.old.length == 0)) return 'Add object';
@@ -33,7 +33,7 @@ var format = {
         else if (obj.type == 'sys.login' && obj.new && obj.new.length > 0 && (!obj.old || !obj.old.length == 0)) return 'User ' + obj.new[0].id + ' has came website';
         else if (obj.type == 'sys.login' && obj.new && obj.new.length > 0 && obj.old && obj.old.length > 0) return 'User ' + obj.new[0].id + ' update details';
         else if (obj.type == 'sys.login' && (!obj.new || obj.new.length == 0) && obj.old && obj.old.length > 0) return 'User ' + obj.new[0].id + ' has left website';
-        else if (obj.type == 'sys.grant' && obj.new && obj.new.length > 0 && (!obj.old || !obj.old.length == 0)) return 'Add premission ' + obj.new[0].id;
+        else if (obj.type == 'sys.grant' && obj.new && obj.new.length > 0 && (!obj.old || !obj.old.length == 0)) return 'Add permission ' + obj.new[0].id;
         else if (obj.type == 'sys.grant' && obj.new && obj.new.length > 0 && obj.old && obj.old.length > 0) return 'Edit permission ' + obj.new[0].id;
         else if (obj.type == 'sys.grant' && (!obj.new || obj.new.length == 0) && obj.old && obj.old.length > 0) return 'Delete permission ' + obj.new[0].id;
         else if (obj.type == 'sys.operation' && obj.new && obj.new.length > 0 && (!obj.old || !obj.old.length == 0)) return 'Add object ' + obj.new[0].id;
@@ -46,10 +46,37 @@ var format = {
         else if (obj.type == 'sys.role' && obj.new && obj.new.length > 0 && obj.old && obj.old.length > 0) return 'Edit role ' + obj.new[0].id;
         else if (obj.type == 'sys.role' && (!obj.new || obj.new.length == 0) && obj.old && obj.old.length > 0) return 'Delete role ' + obj.new[0].id;
         else return obj.type;
+    },
+    getOperationsDescription(obj){
+        if (obj.type == 'sys.signup') return 'User ' + obj.new[0].id + ' details: ' + JSON.stringify(obj.new[0].details);
+        else if (obj.type == 'sys.login' && obj.new && obj.new.length > 0 && (!obj.old || !obj.old.length == 0)) return 'User ' + obj.new[0].id + ' has came website';
+        else if (obj.type == 'sys.login' && obj.new && obj.new.length > 0 && obj.old && obj.old.length > 0) return 'User ' + obj.new[0].id + ' update details';
+        else if (obj.type == 'sys.login' && (!obj.new || obj.new.length == 0) && obj.old && obj.old.length > 0) return 'User ' + obj.new[0].id + ' has left website';
+        else if (obj.type == 'sys.grant' && obj.new && obj.new.length > 0 && (!obj.old || !obj.old.length == 0)) return 'Add permission ' + obj.new[0].id;
+        else if (obj.type == 'sys.grant' && obj.new && obj.new.length > 0 && obj.old && obj.old.length > 0) return 'Edit permission ' + obj.new[0].id;
+        else if (obj.type == 'sys.grant' && (!obj.new || obj.new.length == 0) && obj.old && obj.old.length > 0) return 'Delete permission ' + obj.new[0].id;
+        else if (obj.type == 'sys.operation' && obj.new && obj.new.length > 0 && (!obj.old || !obj.old.length == 0)) return 'Add object ' + obj.new[0].id;
+        else if (obj.type == 'sys.operation' && obj.new && obj.new.length > 0 && obj.old && obj.old.length > 0) return 'Edit object ' + obj.new[0].id;
+        else if (obj.type == 'sys.operation' && (!obj.new || obj.new.length == 0) && obj.old && obj.old.length > 0) return 'Delete object ' + obj.new[0].id;
+        else if (obj.type == 'sys.validate' && obj.new && obj.new.length > 0 && (!obj.old || !obj.old.length == 0)) return 'Add validator ' + obj.new[0].id;
+        else if (obj.type == 'sys.validate' && obj.new && obj.new.length > 0 && obj.old && obj.old.length > 0) return 'Edit validator ' + obj.new[0].id;
+        else if (obj.type == 'sys.validate' && (!obj.new || obj.new.length == 0) && obj.old && obj.old.length > 0) return 'Delete validator ' + obj.new[0].id;
+        else if (obj.type == 'sys.role' && obj.new && obj.new.length > 0 && (!obj.old || !obj.old.length == 0)) return 'Add role ' + obj.new[0].id;
+        else if (obj.type == 'sys.role' && obj.new && obj.new.length > 0 && obj.old && obj.old.length > 0) return 'Edit role ' + obj.new[0].id;
+        else if (obj.type == 'sys.role' && (!obj.new || obj.new.length == 0) && obj.old && obj.old.length > 0) return 'Delete role ' + obj.new[0].id;
+        else return obj.type;
+    },
+    getObjectName(obj){
+        if (obj.type == 'sys.signup') return 'User signup'
+        else if (obj.type == 'sys.login') return 'User login';
+        else if (obj.type == 'sys.grant') return 'Permission';
+        else if (obj.type == 'sys.operation') return 'Object';
+        else if (obj.type == 'sys.validate') return 'Validator';
+        else if (obj.type == 'sys.role') return 'Role';
+        else return obj.type;
     }
 };
 
-//
 // getObjectDescription(var obj)
 //
 // getObjectName(var obj)
