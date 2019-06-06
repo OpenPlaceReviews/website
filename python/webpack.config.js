a2779f357e9f62d1dad1614a10edf7c329c03e2f
status <sinhropazatron@gmail.com>
2019-06-06 19:14:56 +0300

    add mobile menu

--- python/webpack.config.js
+++ python/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
         })
       },
       {
-        test: /\.(png|jpg)$/,
+        test: /\.(png|jpg|svg)$/,
         loader: 'file-loader',
         options:{
           name: "../images/[name].[ext]",
