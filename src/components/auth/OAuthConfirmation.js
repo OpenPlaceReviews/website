259dd95a00f7a1c78cd2c6dc3c5fef3781ba5f00
Ramenky Underground <ramenky27@gmail.com>
2020-10-23 16:24:11 +0300

    Add OAuth change nickname form

--- src/components/auth/OAuthConfirmation.js
+++ src/components/auth/OAuthConfirmation.js
@@ -1,9 +1,13 @@
 import React, {useEffect, useState} from "react";
 import auth from "../../api/auth";
 import {Redirect} from "react-router-dom";
+import OAuthConfirmationForm from "./OAuthConfirmationForm";
 
 export default ({isLoggedIn, params, onSuccess}) => {
   const [errorMsg, setError] = useState('');
+  const [confirmData, setConfirmData] = useState(null);
+
+  const defaultAlertMsg = "Error while processing request. Please try again later.";
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,45 +17,34 @@ export default ({isLoggedIn, params, onSuccess}) => {
           oauthVerifier: params.oauth_verifier,
           code: params.code,
         });
-        const email = confirmData.details.email;
-        if (email && email.length) {
-          delete confirmData.details.email;
-        }
 
-        const {data: status} = await auth.checkName(confirmData.nickname);
-
-        if (status.blockchain !== 'ok') {
-          await auth.signUp({
-            name: confirmData.nickname,
-            oauthAccessToken: confirmData.accessToken,
-            userDetails: confirmData.details,
-            email,
+        if (confirmData.possibeSignups.length) {
+          const {data: loginData} = await auth.logIn({
+            name: confirmData.possibeSignups[0],
+            oauthAccessToken: confirmData.accessToken
           });
+
+          onSuccess({
+            name: confirmData.possibeSignups[0],
+            token: loginData.eval.privatekey,
+          });
+        } else {
+          setConfirmData(confirmData);
         }
-
-        const {data} = await auth.logIn({
-          name: confirmData.nickname,
-          oauthAccessToken: confirmData.accessToken
-        });
-
-        onSuccess({
-          name: confirmData.nickname,
-          token: data.eval.privatekey,
-        });
       } catch (error) {
-        let errMessage = "Error while processing request. Please try again later.";
+        console.log(error);
         if (error.response && error.response.data) {
-          errMessage = error.response.data.message;
+          setError(error.response.data.message);
+        } else {
+          setError(defaultAlertMsg);
         }
-
-        setError(errMessage);
       }
     };
 
     if (!isLoggedIn && (params.code || params.oauth_token || params.oauth_verifier)) {
       fetchData();
     }
-  }, []);
+  }, [confirmData]);
 
   if (isLoggedIn) {
     return <Redirect to={"/profile"}/>
@@ -62,6 +55,16 @@ export default ({isLoggedIn, params, onSuccess}) => {
       <h1>OAuth login failed</h1>
       <p>{errorMsg}</p>
     </div>
+  }
+
+  if (confirmData) {
+    return <OAuthConfirmationForm
+      oauthNickname={confirmData.oauthNickname}
+      oauthAccessToken={confirmData.accessToken}
+      details={confirmData.details}
+      onSuccess={onSuccess}
+      onError={setError}
+    />
   }
 
   return <div className="auth-container" id="opr-app">
---
+++ src/components/auth/OAuthConfirmationForm.js
@@ -0,0 +1,159 @@
+import React, {useEffect, useRef, useState} from "react";
+
+import Alert from "@material-ui/lab/Alert";
+import {Button, TextField} from "@material-ui/core";
+
+import auth from "../../api/auth";
+
+const TYPING_TIMEOUT = 1000;
+
+let writeTimeout = null;
+export default ({name, oauthAccessToken, userDetails, onSuccess, onError}) => {
+  const [showAlert, setAlert] = useState(null);
+  const [isReady, setReady] = useState(false);
+  const [isSubmit, setSubmit] = useState(false);
+  const [formData, setData] = useState({
+    oauthNickname: {
+      value: name,
+      error: ''
+    },
+  });
+
+  const defaultAlertMsg = "Error while processing request. Please try again later.";
+
+  const handler = (event) => {
+    const { target } = event;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const { name } = target;
+
+    setData( formData => ({
+      ...formData,
+      [name]: {
+        ...formData[name],
+        value
+      }
+    }));
+  };
+
+  const formRef = useRef();
+  useEffect(() => {
+    const unlockForm = () => {
+      let errors = 0;
+      for (let field in formData) {
+        if (formData[field].error.length) {
+          errors++;
+        }
+      }
+
+      setReady(errors === 0 && formRef.current.checkValidity());
+    };
+
+    unlockForm();
+  }, [formData, isReady]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await auth.checkName(formData.oauthNickname.value);
+        let error = '';
+        if (data && data["db-name"] === "ok" && data["blockchain"] === 'ok') {
+          error = 'Username already inuse';
+        }
+
+        setData( formData => ({
+          ...formData,
+          oauthNickname: {
+            ...formData.oauthNickname,
+            error,
+          }
+        }));
+
+        setAlert(null);
+      } catch (error) {
+        if (error.response && error.response.data){
+          setAlert(error.response.data.message);
+        } else {
+          setAlert(defaultAlertMsg);
+        }
+      }
+    };
+
+    if (formData.oauthNickname.value.length) {
+      clearTimeout(writeTimeout);
+      writeTimeout = setTimeout(() => {
+        fetchData();
+      }, TYPING_TIMEOUT);
+    }
+  }, [formData.oauthNickname.value]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const email = userDetails.email;
+        if (email && email.length) {
+          delete userDetails.email;
+        }
+
+        await auth.signUp({
+          name: formData.oauthNickname.value,
+          oauthAccessToken,
+          userDetails,
+          email,
+        });
+
+        const {data} = await auth.logIn({
+          name: formData.oauthNickname.value,
+          oauthAccessToken,
+        });
+
+        onSuccess({
+          token: data.eval.privatekey,
+        });
+      } catch (error) {
+        if (error.response && error.response.data){
+          onError(error.response.data.message);
+        } else {
+          onError(defaultAlertMsg);
+        }
+      }
+
+      setSubmit(false);
+    };
+
+    if (isSubmit) {
+      fetchData();
+    }
+  }, [isSubmit]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setSubmit(true);
+  };
+
+  return <div className="auth-container" id="opr-app">
+    <form className="signup-form" autoComplete="off" onSubmit={onSubmit}>
+      {showAlert && <Alert
+        className="form-alert"
+        severity="error">
+        {showAlert}
+      </Alert>}
+
+      <div className="form-item">
+        <TextField
+          name="oauthNickname"
+          required={true}
+          label="Nickname"
+          placeholder="Enter a nickname"
+          onChange={handler}
+          value={formData.oauthNickname.value}
+          error={formData.oauthNickname.error.length > 0}
+          helperText={formData.oauthNickname.error ? formData.oauthNickname.error : ''}
+          variant="outlined"
+          fullWidth={true}
+        />
+      </div>
+
+      <Button variant="outlined" type="submit" color="primary" disabled={isReady !== false}>Send</Button>
+    </form>
+  </div>;
+}
