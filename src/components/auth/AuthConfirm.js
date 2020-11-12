2cf0ed5bcc66dd25fa637f096c50c7f78fed42c4
Ramenky Underground <ramenky27@gmail.com>
2020-11-12 07:31:12 +0200

    Fix redirects to profile

--- src/components/auth/AuthConfirm.js
+++ src/components/auth/AuthConfirm.js
@@ -14,7 +14,7 @@ export default ({location}) => {
   const {op, name, token, oauth_token, oauth_verifier, code} = qs.parse(location.search.substring(1));
   const isConfirmation = (name && token && op);
   const isOAuth = (oauth_token || oauth_verifier || code);
-  const isLoggedIn = (authData.name && authData.name.length);
+  const isLoggedIn = (authData.token && authData.token.length);
   const force_signup = storage.get('opr-force-signup');
 
   if (isLoggedIn) {
@@ -22,11 +22,17 @@ export default ({location}) => {
   } else if (isConfirmation) {
     const params = {name, token, op};
     if (op === 'signup_confirm') {
+      if (authData.name && authData.name.length) {
+        return <Redirect to="/profile"/>;
+      }
       return <EmailConfirmation isLoggedIn={isLoggedIn} params={params} onSuccess={logIn}/>
     } else if (op === 'reset_pwd') {
       return <ResetPwdConfirmation params={params}/>
     }
   } else if (isOAuth) {
+    if (authData.name && authData.name.length) {
+      return <Redirect to="/profile"/>;
+    }
     const params = {oauth_token, oauth_verifier, code, force_signup};
     return <OAuthConfirmation isLoggedIn={isLoggedIn} params={params} onSignUp={signUp} onLogIn={logIn}/>
   }
