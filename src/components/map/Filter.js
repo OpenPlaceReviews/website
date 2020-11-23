afa2b5c84384553dbb281b4dbb0667859fe7638b
Ramenky Underground <ramenky27@gmail.com>
2020-11-23 12:36:05 +0200

    Add sidebar component

--- src/components/map/Filter.js
+++ src/components/map/Filter.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import {Select, MenuItem} from '@material-ui/core';
 
 export default (options, onSelect) => {
-
+  return <div className="map-filter">
+    <p className="map-filter-header">Filter places</p>
+    <Select
+      className="map-filter-select"
+      onChange={onSelect}
+      placeholder="Select category"
+      fullWidth={true}
+      disableUnderline={true}
+      label="Category"
+    >
+      {options.length && options.map((option, i) => <MenuItem value={option} key={i} className="map-filter-option">{option}</MenuItem>)}
+    </Select>
+  </div>
 };
 
--- src/components/map/StatusBar.js
+++ src/components/map/StatusBar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
 export default ({status}) => {
-  console.log('STATUS BAR IS RENDER');
   return <p className="map-status">{status}</p>;
 };
