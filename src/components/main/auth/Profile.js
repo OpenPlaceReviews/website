1dccee4fae7a9b0fb9e43f8fc56a085fd7e9a081
Ramenky Underground <ramenky27@gmail.com>
2020-12-24 16:10:03 +0200

    Fix syntax error unresolved Link in Profile.js

--- src/components/main/auth/Profile.js
+++ src/components/main/auth/Profile.js
@@ -1,5 +1,5 @@
 import React, {useContext, useEffect, useState} from 'react';
-import {Redirect} from "react-router-dom";
+import {Redirect, Link} from "react-router-dom";
 
 import {UserContext} from "../../../context";
 import EmailConfirmation from "./EmailConfirmation";
