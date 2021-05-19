94c5d4c8ab204df0788fdafaca6de9a1de1cec00
vshcherb <victor.shcherb@gmail.com>
2021-05-19 13:44:51 +0200

    Update LandingRouter.js

--- src/components/landing/LandingRouter.js
+++ src/components/landing/LandingRouter.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Route, Switch, useRouteMatch} from "react-router-dom";
 
-import LendingPage from "./LendingPage";
+import LandingPage from "./LandingPage";
 
 export default function LandingRouter () {
   let { path } = useRouteMatch();
 
   return <Switch>
-    <Route path={path} component={LendingPage} />
+    <Route path={path} component={LandingPage} />
   </Switch>
 };
