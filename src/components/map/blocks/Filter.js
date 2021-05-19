8e787e16511bb2e2b72bfc1ed78e5533ae93120e
alisa911 <alisa.91150@gmail.com>
2021-05-19 14:53:43 +0300

    fix filter for none

--- src/components/map/blocks/Filter.js
+++ src/components/map/blocks/Filter.js
@@ -255,9 +255,7 @@ export default ({ isLoggedIn, placeTypes, onCategorySelect, taskSelection, onTas
             minDate={selectedDates.startDate}
           />
         </div>}
-      </>}
-      {isLoggedIn && selectedTask && <>
-        <div className={classes.switch}>
+        {<div className={classes.switch}>
           <span>Display reviewed places</span>
           <Switch
               className={classes.position}
@@ -268,7 +266,7 @@ export default ({ isLoggedIn, placeTypes, onCategorySelect, taskSelection, onTas
                 checked: classes.checked
               }}
               value={selectedReviewedPlacesVisible} onClick={toggleReviewedPlacesVisible}/>
-        </div>
+        </div>}
       </>}
     </div>
   </div>
