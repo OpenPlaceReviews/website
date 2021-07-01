df78e9d6a2b45083b8005940d0910f409a8bd98a
alisa911 <alisa.91150@gmail.com>
2021-07-01 14:50:20 +0300

    add red color for deleted places

--- src/components/map/MarkerEntity.js
+++ src/components/map/MarkerEntity.js
@@ -1,13 +1,15 @@
 import React from "react";
 import {Marker} from "react-leaflet";
-import MarkerIcon from './MrkerIcon';
+import MarkerIcon from './MarkerIcon';
 
 export default function ({feature, onSelect}) {
   const {place_type} = feature.properties;
+  const {deleted} = feature.properties.deleted;
+  const {place_deleted_osm} = feature.properties.place_deleted_osm;
   const lngLat = feature.geometry.coordinates;
   const latLng = [lngLat[1], lngLat[0]];
 
-  const icon = MarkerIcon(place_type);
+  const icon = MarkerIcon(place_type, deleted, place_deleted_osm);
 
   const onClickHandler = () => {
     onSelect(feature);
