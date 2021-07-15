d3972f467e775bf7f03fb418adc0d431db58bbb0
alisa911 <alisa.91150@gmail.com>
2021-07-15 14:42:32 +0300

    fix merge dialog

--- src/components/map/MergeList.js
+++ src/components/map/MergeList.js
@@ -1,17 +1,14 @@
 import MergeListDialog from "./blocks/dialogs/MergeListDialog";
 import React, {useEffect, useState} from "react";
-import Utils from "../util/Utils";
 
 export default function MergeList({mergeListDialogOpen, mergePlaces, placeTypes, setMergeListDialogOpen}) {
 
     const [idsPlacesCache, setIdsPlacesCache] = useState([]);
-    const prevMergePlaces = Utils.usePrevious(mergePlaces);
-    const placesChanged = prevMergePlaces && (JSON.stringify(mergePlaces) !== JSON.stringify(prevMergePlaces));
 
     useEffect(() => {
         idsPlacesCache.splice(0, idsPlacesCache.length)
         setIdsPlacesCache(idsPlacesCache);
-    }, [placesChanged]);
+    }, [mergePlaces]);
 
     return mergeListDialogOpen &&
         <MergeListDialog mergePlaces={mergePlaces} placeTypes={placeTypes} mergeListDialogOpen={mergeListDialogOpen}
